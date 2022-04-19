943b07b600dfe94b80f391eda6447232458eb93a
Андрій Шевельов <ashevelevmti@gmail.com>
2022-04-19 22:21:59 +0300

    add

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import SectionCard from './section01/card'
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        textTransform: 'uppercase',
+        color: '#010101',
+      }}
+    >
+      <SectionCard/>
+    </div>
+  );
+};
---
+++ src/components/section01/card.js
@@ -0,0 +1,24 @@
+import Card from './cardPr';
+import infoProf from '../user.json';
+const {
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+} = infoProf;
+
+export default function SectionCard() {
+  return (
+    <Card
+      key="1"
+      username={username}
+      tag={tag}
+      location={location}
+      avatar={avatar}
+      followers={followers}
+      views={views}
+      likes={likes}
+    />
+  );
+}
